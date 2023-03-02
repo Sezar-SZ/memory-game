d3592a65c845b9149c4bf3c5b2a1ee5932f6d70e
Sezar-SZ <soheilzarepoor79@gmail.com>
2023-03-02 18:17:42 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="fullScreenContainer">Hello, World!</div>
+</template>
+
+<style scoped>
+/* TODO: Rewrite in bootstrap */
+.fullScreenContainer {
+  width: 100vw;
+  height: 100vh;
+  background-color: #111;
+}
+</style>
